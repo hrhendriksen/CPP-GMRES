eae28d40d396b26dda4c0711bfac10869feb8ed9
Hidde Hendriksen <h.hendriksen@hotmail.com>
2018-06-05 15:09:36 +0100

    Started with GMRES

--- gmres.cpp
+++ gmres.cpp
@@ -3,10 +3,11 @@
 
 Vector gmres(const Matrix& A, Vector& b, Vector& x0, int max_it, double tol)
 {
-	assert(A.GetNumberofRows() == A.GetNumberofColumns());
+    int n = A.GetNumberofRows();
+	assert(n == A.GetNumberofColumns());
 
-	int iter = 0;
-
+	int iter = 1;
+	double residual = 5.0;
 	// Calculate initial residual r0
 	Vector r0 = b - A*x0;
 	double norm_r0 = norm(r0);
@@ -18,8 +19,22 @@ Vector gmres(const Matrix& A, Vector& b, Vector& x0, int max_it, double tol)
 
 	Vector v = r0/norm_r0;
 
-	//Now build the matrix V using the previously found v's.
-	Matrix V(v);
-	print(V);
-	return x0;
+	Vector v_iter(n);
+
+	while(residual > tol && iter < max_it)
+	{
+		Matrix H(iter, iter+1);
+	 // Do step k of Arnoldi
+		Vector w = A * v;
+		for (int j = 1; j < iter; ++j)
+		{
+			continue;
+		}
+
+
+
+
+		iter +=1;
+	}
+	return v;
 }
